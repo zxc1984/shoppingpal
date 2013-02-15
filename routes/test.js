e47fbbeb5befc803e6724e94aa2de37eef39e52d
Jagdish U.C <jagdish.uc@gmail.com>
2013-02-15 15:12:44 +0800

    mongo stuff

--- routes/test.js
+++ routes/test.js
@@ -1,92 +1,10 @@
-var databaseUrl = "test:test@ds051007.mongolab.com:51007/shoppal"; // "username:password@example.com/mydb"
-var collections = ["shoppal"]
-var db = require("mongojs").connect(databaseUrl, collections);
+module.exports= function(app) {
+
+    app.get('/api/dashboard', function(req, res) {
+        return res.send("hello");
+    });
+
+     app.get('/list/insert', list.insert);
+}
+
  
-var objectId = function (_id) {
-    if (_id.length === 24 && parseInt(db.ObjectId(_id).getTimestamp().toISOString().slice(0,4), 10) >= 2010) {
-        return db.ObjectId(_id);
-    } 
-    return _id;
-}
-
-//Function callback
-var fn = function (req, res) {
-    res.contentType('application/json');
-    var fn = function (err, doc) { 
-        if (err) { 
-            if (err.message) {
-                doc = {error : err.message} 
-            } else {
-                doc = {error : JSON.stringify(err)} 
-            }
-        }
-        if (typeof doc === "number" || req.params.cmd === "distinct") { doc = {ok : doc}; } 
-        res.send(doc); 
-    };
-    return fn;
-};
-
-
-exports.query = function(req, res) {
-    var item, sort = {}, qw = {};
-    for (item in req.query) {
-        req.query[item] = (typeof +req.query[item] === 'number' && isFinite(req.query[item]))
-            ? parseFloat(req.query[item],10) 
-            : req.query[item];
-        if (item != 'limit' && item != 'skip' && item != 'sort' && item != 'order' && req.query[item] != "undefined" && req.query[item]) {
-            qw[item] = req.query[item]; 
-        }
-    }  
-    if (req.query.sort) { sort[req.query.sort] = (req.query.order === 'desc' || req.query.order === -1) ? -1 : 1; }
-    db.collection(req.params.collection).find(qw).sort(sort).skip(req.query.skip).limit(req.query.limit).toArray(fn(req, res))
-};
-
-exports.findAll = function(req, res) {
-	qw = {};
-	db.collection(req.params.collection).find(qw).toArray(fn(req, res));
-};
-
-exports.findBy = function(req, res) {
-    qw = {};
-    filter = req.params.filter; 
-    qw[filter] = req.params.key;
-    db.collection(req.params.collection).find(qw).toArray(fn(req, res));
-};
-
-// Save
-exports.insert = function(req, res) {
-    qw = {"name":"whatsup"};
-    db.collection(req.params.collection).insert(qw, {safe:true}, fn(req, res));
-};
-
-
-// Save
-exports.save = function(req, res) {
-    if (req.body._id) { req.body._id = objectId(req.body._id);}
-    db.collection(req.params.collection).save(req.body, {safe:true}, fn(req, res));
-};
-
-// Delete
-exports.delete = function(req, res) {
-    db.collection(req.params.collection).remove({_id:objectId(req.params.id)}, {safe:true}, fn(req, res));
-};
-
-//Group
-exports.groupBy = function(req, res) {
-    db.collection(req.params.collection).group(req.body.keys, req.body.cond, req.body.initial, req.body.reduce, req.body.finalize, fn(req, res))
-};
-
-// MapReduce
-exports.mapReduce = function(req, res) {
-    if (!req.body.options) {req.body.options  = {}};
-    req.body.options.out = { inline : 1};
-    req.body.options.verbose = false;
-    db.collection(req.params.collection).mapReduce(req.body.map, req.body.reduce, req.body.options, fn(req, res));    
-};
-
-// Command (count, distinct, find, aggregate)
-exports.cmd =  function (req, res) {
-    if (req.params.cmd === 'distinct') {req.body = req.body.key}
-    db.collection(req.params.collection)[req.params.cmd](req.body, fn(req, res)); 
-}
-
