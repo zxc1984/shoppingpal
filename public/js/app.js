099c11ead04c6b715c3d3f5699d1aa90d0e13906
zxc1984 <zhengbin.hu.2010@sis.smu.edu.sg>
2013-02-16 17:58:28 +0800

    update

--- public/js/app.js
+++ public/js/app.js
@@ -6,8 +6,10 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives'])
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1', controller: MyCtrl1});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2', controller: MyCtrl2});
-    $routeProvider.when('/lists', {templateUrl: 'partials/lists', controller: ListCtrl});
-    $routeProvider.when('/lists/update', {templateUrl: 'partials/createListName', controller: ListCtrl});
-    $routeProvider.otherwise({redirectTo: '/lists'});
+    $routeProvider.when('/list', {templateUrl: 'partials/lists', controller: ListCtrl});
+    $routeProvider.when('/list/create', {templateUrl: 'partials/listCreate', controller: ListCtrl});
+    $routeProvider.when('/list/addFriends', {templateUrl: 'partials/listAddFriends', controller: ListCtrl});
+    $routeProvider.when('/list/update', {templateUrl: 'partials/listUpdate', controller: ListCtrl});
+    $routeProvider.otherwise({redirectTo: '/list'});
     $locationProvider.html5Mode(true);
   }]);
