6b5b10484f19cba317ce1274481575f0af577726
zxc1984 <zhengbin.hu.2010@sis.smu.edu.sg>
2013-02-19 15:48:29 +0800

    views added

--- public/js/app.js
+++ public/js/app.js
@@ -4,21 +4,42 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2', controller: MyCtrl2});
+    $routeProvider.when('/', {templateUrl: 'partials/m_welcome', controller: MyCtrl1});
+    $routeProvider.when('/login', {templateUrl: 'partials/m_login', controller: MyCtrl2});
+    $routeProvider.when('/register', {templateUrl: 'partials/m_register', controller: MyCtrl2});
+    $routeProvider.when('/forget-password', {templateUrl: 'partials/m_forgetPassword', controller: MyCtrl2});
     
     // List Scenario
     $routeProvider.when('/list', {templateUrl: 'partials/lists', controller: ListCtrl});
     $routeProvider.when('/list/create', {templateUrl: 'partials/listCreate', controller: ListCtrl});
     $routeProvider.when('/list/addFriends', {templateUrl: 'partials/listAddFriends', controller: ListCtrl});
-    $routeProvider.when('/list/update', {templateUrl: 'partials/listUpdate', controller: ListCtrl});
+    $routeProvider.when('/list/1', {templateUrl: 'partials/listDetails', controller: ListCtrl});
+    $routeProvider.when('/list/1/itemdetail/1', {templateUrl: 'partials/listItemDetail', controller: ListCtrl});
+    $routeProvider.when('/list/1/option', {templateUrl: 'partials/listSetting', controller: ListCtrl});
     
     // Add Items Scenario
-    $routeProvider.when('/list/update/itemlist/', {templateUrl: 'partials/itemLists', controller: ListCtrl});
-
-    // Expenditure Scenario
+    $routeProvider.when('/list/1/additem/', {templateUrl: 'partials/items', controller: ItemCtrl});
+    $routeProvider.when('/list/1/additem/create', {templateUrl: 'partials/itemCreate', controller: ItemCtrl});
+    $routeProvider.when('/list/1/additem/1', {templateUrl: 'partials/itemCreate', controller: ItemCtrl});
 
     // Shopping Scenario
-    $routeProvider.otherwise({redirectTo: '/list'});
+    $routeProvider.when('/shopping', {templateUrl: 'partials/shopping', controller: ListCtrl});
+    $routeProvider.when('/shopping/on', {templateUrl: 'partials/shopping', controller: ListCtrl});
+    $routeProvider.when('/shopping/checkout', {templateUrl: 'partials/shoppingCheckout', controller: ListCtrl});
+
+    // Expenditure Scenario
+    $routeProvider.when('/expense', {templateUrl: 'partials/expenseSummary', controller: ExpenseCtrl});
+    $routeProvider.when('/expense/friends', {templateUrl: 'partials/expenseFriends', controller: ExpenseCtrl});
+    $routeProvider.when('/expense/details/1', {templateUrl: 'partials/expenseDetails', controller: ExpenseCtrl});
+
+    // payment scenario
+    $routeProvider.when('/payment/', {templateUrl: 'partials/paymentSummary', controller: ListCtrl});
+    $routeProvider.when('/payment/new', {templateUrl: 'partials/paymentCreate', controller: ListCtrl});
+    $routeProvider.when('/payment/confirmation', {templateUrl: 'partials/paymentConfirmation', controller: ListCtrl});
+    $routeProvider.when('/payment/invoice', {templateUrl: 'partials/paymentDetails', controller: ListCtrl});
+    $routeProvider.when('/payment/details/1', {templateUrl: 'partials/paymentDetails', controller: ListCtrl});
+
+    // Default
+    $routeProvider.otherwise({redirectTo: '/'});
     $locationProvider.html5Mode(true);
   }]);
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,6 +1,6 @@
 /* Controllers */
 var st = sidetap();
-function AppCtrl($scope, $http) {
+function AppCtrl($scope, $http, $location) {
   $http({method: 'GET', url: '/api/name'}).
   success(function(data, status, headers, config) {
     $scope.name = data.name;
@@ -10,6 +10,26 @@ function AppCtrl($scope, $http) {
   });
   $scope.menuClick = function() {
     st.toggle_nav();
+  };
+  $scope.loadRegisterPage = function() {
+    $location.path('/register');
+  };
+  $scope.loadLoginPage = function() {
+    $location.path('/login');
+  };
+  $scope.register = function() {
+    $location.path('/list/');
+  };
+  $scope.login = function() {
+    $location.path('/list/');
+  };
+
+}
+
+function UtilCtrl() {
+  $scope.clearListName = function() {
+    if ($scope.list)
+      $scope.list.name = "";
   };
 }
 
@@ -22,11 +42,24 @@ function MyCtrl2() {
 MyCtrl2.$inject = [];
 
 function ListCtrl($scope, $http,$location) {
-	$scope.clearListName = function() {
-		if ($scope.list)
-			$scope.list.name = "";
-	};
+  $scope.clearListName = function() {
+    if ($scope.list)
+      $scope.list.name = "";
+  };
   $scope.ListDetails = function() {
-    $location.path('/list/update/');
+    $location.path('/list/1');
+  }
+  $scope.ListItemDetail = function() {
+    $location.path('/list/1/itemdetail/1');
+  }
+}
+function ItemCtrl($scope, $http,$location) {
+  $scope.ItemDetails = function() {
+    $location.path('/list/1/additem/1');
+  }
+}
+function ExpenseCtrl($scope, $http, $location) {
+  $scope.ExpenseDetail = function() {
+    $location.path('/expense/details/1');
   }
 }
